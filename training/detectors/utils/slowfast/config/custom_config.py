1bdcea24483c976170c1355a32a4a1309a817e37
Shubham Dadyal <shubhamdadyal09@gmail.com>
2026-05-11 18:49:23 +0530

    Initial import

--- training/detectors/utils/slowfast/config/custom_config.py
+++
@@ -1,9 +0,0 @@
-                      
-                                                                       
-
-                                           
-
-
-def add_custom_config(_C):
-                                      
-    pass
--- training/detectors/utils/slowfast/models/__init__.py
+++
@@ -1,6 +0,0 @@
-                      
-                                                                       
-
-from .build import MODEL_REGISTRY, build_model        
-from .custom_video_model_builder import *        
-from .video_model_builder import ResNet, SlowFast        
